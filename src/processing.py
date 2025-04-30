0b36073149eee42440523444f14c89b5b6f87973
Rustam Gabdelislamov <rustamgabdelislamov911@gmail.com>
2025-04-30 13:03:57 +0500

    add test_proccessing.py

--- src/processing.py
+++ src/processing.py
@@ -2,8 +2,13 @@
 def filter_by_state(list_dict: list[dict], state: str = 'EXECUTED') -> list[dict]:
     """Функция которая возвращает список словарей с парметром 'state'"""
 
-    filter_by_state_ = [el for el in list_dict if el.get('state') == state]
-    return sort_by_date(filter_by_state_)
+    if isinstance(list_dict, list):
+        filter_by_state_ = [el for el in list_dict if el.get('state') == state]
+        if not filter_by_state_ and list_dict == []:
+            return []
+        return sort_by_date(filter_by_state_)
+    return 'Вы ввели не список'
+
 
 
 def sort_by_date(filter_by_state_: list[dict], reverse: bool = True) -> list[dict]:
